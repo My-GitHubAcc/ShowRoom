12865b6db3a02fe56014d562dea85171ca14fe35
Adwin <23533932+dev-gitXplor@users.noreply.github.com>
2018-09-01 19:17:23 +0100

    Repository Base Class

---
+++ ShowRoom/Models/Repository/Base/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowRoom.Models.Repository
+{
+    interface IRepository<TEntity>
+    {
+        IQueryable<TEntity> All { get; }
+
+        TEntity Find(params object[] keyvalues);
+        IQueryable<TEntity> FindBy(Expression<Func<TEntity, bool>> predicate);
+
+        void Add(TEntity entity);
+        void Edit(TEntity entity);
+        void Delete(TEntity entity);
+
+        void Save();
+    }
+}
---
+++ ShowRoom/Models/Repository/Base/Repository.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace ShowRoom.Models.Repository
+{
+    public class Repository<TEntity, TContext> : IRepository<TEntity>
+        where TEntity : class
+        where TContext : DbContext, new()
+    {
+        TContext _entities = new TContext();
+        public IQueryable<TEntity> All
+        {
+            get
+            {
+                return _entities.Set<TEntity>();
+            }
+        }
+
+        public void Add(TEntity entity)
+        {
+            try
+            {
+                _entities.Set<TEntity>().Add(entity);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            try
+            {
+                _entities.Set<TEntity>().Remove(entity);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void Edit(TEntity entity)
+        {
+            try
+            {
+                _entities.Entry(entity).State = EntityState.Modified;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public TEntity Find(params object[] keyValues)
+        {
+            try
+            {
+                return _entities.Set<TEntity>().Find(keyValues);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public IQueryable<TEntity> FindBy(Expression<Func<TEntity, bool>> predicate)
+        {
+            try
+            {
+                return All.Where(predicate);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void Save()
+        {
+            _entities.SaveChanges();
+        }
+    }
+}
