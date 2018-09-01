7516eb9a640239297c3e40a383d44a08e95f99e1
Adwin <23533932+dev-gitXplor@users.noreply.github.com>
2018-09-01 21:24:01 +0100

    Basic Models

---
+++ ShowRoom/Models/MarqueList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShowRoom.Models
+{
+    public class MarqueList : List<Marque>
+    {
+        
+    }
+}
---
+++ ShowRoom/Models/ModelList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShowRoom.Models
+{
+    public class ModelList : List<Model>
+    {
+    }
+}
