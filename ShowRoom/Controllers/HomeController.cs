f20521753fe5a9a574bb1c0adb24da305bfbb064
Adwin <dev_AccLive@users.noreply.github.com>
2018-09-28 18:31:39 +0100

    Base .NET Core Project

---
+++ ShowRoom/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShowRoom.Models;
+
+namespace ShowRoom.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
